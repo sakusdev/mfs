b53ae42719d1e261e7c42c553636aa1b912a0e98
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-07-27 02:20:43 +0900

    Run chord and melody analysis together

--- src/audio/analysis-worker.ts
+++ src/audio/analysis-worker.ts
@@ -1,14 +1,18 @@
 /// <reference lib="webworker" />
 
-import init, { analyze_audio } from "../wasm/pkg/mfs_core";
+import initChord, { analyze_audio } from "../wasm/pkg/mfs_core";
+import initMelody, { analyze_melody } from "../wasm/melody-pkg/melody_core";
 
 type Input = { samples: Float32Array; sampleRate: number };
 
 self.onmessage = async (event: MessageEvent<Input>) => {
   try {
-    await init();
-    const json = analyze_audio(event.data.samples, event.data.sampleRate);
-    self.postMessage({ type: "result", result: JSON.parse(json) });
+    await Promise.all([initChord(), initMelody()]);
+    const chordJson = analyze_audio(event.data.samples, event.data.sampleRate);
+    const melodyJson = analyze_melody(event.data.samples, event.data.sampleRate);
+    const result = JSON.parse(chordJson);
+    result.melody = JSON.parse(melodyJson);
+    self.postMessage({ type: "result", result });
   } catch (error) {
     self.postMessage({
       type: "error",
